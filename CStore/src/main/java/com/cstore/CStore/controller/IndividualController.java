59ec149437ea9cd0695e36c9ea3e41dedc10daa7
ssandeepkumar <ssandeepkumar@USBLRSSANDEEPK1.us.deloitte.com>
2019-07-01 15:04:20 +0530

    committed the entire project

---
+++ CStore/src/main/java/com/cstore/CStore/controller/IndividualController.java
@@ -0,0 +1,11 @@
+package com.cstore.CStore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ind")
+public class IndividualController {
+	
+
+}//IndividualController
